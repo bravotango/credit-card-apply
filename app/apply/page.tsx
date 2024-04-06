aa65782563f39c63adcea85ab254492be6127c66
seattleriot <seattleriot@hotmail.com>
2024-04-06 01:22:56 -0700

    Getting Vercel running

--- app/apply/page.tsx
+++ app/apply/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React, { useState } from 'react';
 import Wizard from '../components/wizard';
-import GlobalState from '../components/DisplayStore';
+import GlobalState from '../components/displayStore';
 import {
   Address,
   Congratulations,
