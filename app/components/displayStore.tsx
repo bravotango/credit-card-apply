e3335fd3a68c58fe4576205ad263fc689b4ff0e2
seattleriot <seattleriot@hotmail.com>
2024-11-15 10:41:16 -0800

    Reverting to only logo

--- app/components/displayStore.tsx
+++ app/components/displayStore.tsx
@@ -1,42 +1,16 @@
-'use client';
-
-import React, { Suspense } from 'react';
+import React from 'react';
 import { useGlobalContext } from '../Context/store';
-import { useSearchParams } from 'next/navigation';
 
 const DisplayStore = () => {
   const { payload, wizard } = useGlobalContext();
-
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <SearchParamsComponent payload={payload} wizard={wizard} />
-    </Suspense>
-  );
-};
-
-const SearchParamsComponent = ({
-  payload,
-  wizard,
-}: {
-  payload: any;
-  wizard: any;
-}) => {
-  const searchParams = useSearchParams();
-  const showPayload = searchParams.get('payload') !== '0';
-  const showSteps = searchParams.get('steps') !== '0';
-
   return (
     <>
-      {showPayload && (
-        <code className='code-container'>
-          Payload: {JSON.stringify(payload, null, 2)}
-        </code>
-      )}
-      {showSteps && (
-        <code className='code-container'>
-          Wizard: {JSON.stringify(wizard, null, 2)}
-        </code>
-      )}
+      <code className='code-container'>
+        Payload:{JSON.stringify(payload, null, 2)}
+      </code>
+      <code className='code-container'>
+        Wizard:{JSON.stringify(wizard, null, 2)}
+      </code>
     </>
   );
 };
