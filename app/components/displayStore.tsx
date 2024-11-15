21b1f375e1bf3b818e6302fb0183bedeee1a1398
seattleriot <seattleriot@hotmail.com>
2024-11-15 14:11:18 -0800

    Hide Store

--- app/components/displayStore.tsx
+++ app/components/displayStore.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
 import { useGlobalContext } from '../Context/store';
+import { Suspense } from 'react';
+import { useSearchParams } from 'next/navigation';
 
 const DisplayStore = () => {
   const { payload, wizard } = useGlobalContext();
+  const searchParams = useSearchParams();
+  const showPayload = searchParams.get('noPayload') ?? true;
+  const showSteps = searchParams.get('noSteps') ?? true;
+
   return (
-    <>
-      <code className='code-container'>
-        Payload:{JSON.stringify(payload, null, 2)}
-      </code>
-      <code className='code-container'>
-        Wizard:{JSON.stringify(wizard, null, 2)}
-      </code>
-    </>
+    <Suspense>
+      {showPayload && (
+        <code className='code-container'>
+          Payload:{JSON.stringify(payload, null, 2)}
+        </code>
+      )}
+      {showSteps && (
+        <code className='code-container'>
+          Wizard:{JSON.stringify(wizard, null, 2)}
+        </code>
+      )}
+    </Suspense>
   );
 };
 
