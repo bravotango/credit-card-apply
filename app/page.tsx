62fc81681d0bb1f54650a62b63fc0edaa50a16bd
seattleriot <seattleriot@hotmail.com>
2024-11-15 15:16:17 -0800

    Vercel Suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 'use client';
-import React from 'react';
+import React, { Suspense } from 'react';
 import strings from './strings.json';
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
 
-const Page = () => {
+const PageContent = () => {
   const searchParams = useSearchParams();
 
   // Check if the parameters exist in the query string
@@ -20,18 +20,27 @@ const Page = () => {
   ]
     .filter(Boolean) // Remove null values
     .join('&');
+
+  return (
+    <main className='container stack'>
+      <h1>{strings.GetStarted.Heading}</h1>
+      <h2>{strings.GetStarted.SubHeading}</h2>
+      <Link
+        href={`/apply${queryString ? `?${queryString}` : ''}`}
+        className='button'
+      >
+        {strings.GetStarted.ButtonText}
+      </Link>
+    </main>
+  );
+};
+
+const Page = () => {
   return (
     <div>
-      <main className='container stack'>
-        <h1>{strings.GetStarted.Heading}</h1>
-        <h2>{strings.GetStarted.SubHeading}</h2>
-        <Link
-          href={`/apply${queryString ? `?${queryString}` : ''}`}
-          className='button'
-        >
-          {strings.GetStarted.ButtonText}
-        </Link>
-      </main>
+      <Suspense fallback={<div>Loading...</div>}>
+        <PageContent />
+      </Suspense>
     </div>
   );
 };
