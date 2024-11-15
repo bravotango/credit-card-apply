02b756fb499523e38197746af8eb7982617689e6
seattleriot <seattleriot@hotmail.com>
2024-11-15 13:39:17 -0800

    noLogo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,30 @@
+'use client';
 import Image from 'next/image';
-import type { Metadata } from 'next';
 import { GlobalContextProvider } from './Context/store';
 import './styles/globals.css';
 import Logo from '../public/nordstrom-logo.svg';
 import Head from 'next/head';
-
-export const metadata: Metadata = {
-  title: 'Credit card application',
-  description: 'NextJS 14+',
-};
+import { useSearchParams } from 'next/navigation';
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const searchParams = useSearchParams();
+  const showLogo = searchParams.get('noLogo') ?? true; // default value is "1"
+
   return (
     <html lang='en'>
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
       <body suppressHydrationWarning={true}>
-        <div className='container'>
-          <Image src={Logo} alt='company logo' height='25' className='logo' />
-        </div>
+        {showLogo && (
+          <div className='container'>
+            <Image src={Logo} alt='company logo' height='25' className='logo' />
+          </div>
+        )}
         <GlobalContextProvider>{children}</GlobalContextProvider>
       </body>
     </html>
